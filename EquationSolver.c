a4bdcb362f382f1a9651976a76c10889ad8331aa
Mikhail038 <111287669+Mikhail038@users.noreply.github.com>
2022-08-16 10:42:56 +0300

    Улучшен вввод, перекомпоновка*
    *Переменная, отвечающая за количество решений занесена в стркутуру

--- EquationSolver.c
+++ EquationSolver.c
@@ -14,45 +14,46 @@ struct ASolutions
 {
     float x1;
     float x2;
+    enum Cases n;
 };
 
 typedef struct ASolutions SSolutions;
 
 void coefficients_input (float *a, float *b, float *c);
 int zero_comparation (float x);
-void linear_solver (float b,float c, int *p_SolutionsAmount,SSolutions *p_solution_1);
-void square_solver (float a,float b,float c, int *p_SolutionsAmount,SSolutions *p_solution);
-void solutions_output (int SolutionsAmount, SSolutions solution);
+void linear_solver (float b, float c, SSolutions *p_solution);
+void square_solver (float a, float b, float c, SSolutions *p_solution);
+void solutions_output (SSolutions solution);
 
 
 
 
 int main (void)
 {
-    enum Cases SolutionsAmount;
-
     SSolutions solution;
     float a = 0, b = 0, c = 0;
 
     coefficients_input (&a, &b, &c);
 
-    if (zero_comparation(a) == 0)
-        linear_solver (b, c, &SolutionsAmount, &solution);
+    if (zero_comparation (a) == 0)
+        linear_solver (b, c, &solution);
     else
-        square_solver (a, b, c, &SolutionsAmount, &solution);
+        square_solver (a, b, c, &solution);
 
-    solutions_output (SolutionsAmount,solution);
+    solutions_output (solution);
 
     return 0;
 }
 
 void coefficients_input (float *p_a, float *p_b, float *p_c)
 {
-    printf ("Enter the coefficients of equation: ");
+    printf ("Enter three coefficients of equation: ");
     while (scanf ("%f %f %f", p_a, p_b, p_c) != 3)
     {
-        printf ("Please enter three numbers\n");
-        fflush(stdin);
+        while (getchar() != '\n')
+            printf ("");
+        printf("Please enter three numbers: ");
+        //fflush (stdin);
     }
 }
 
@@ -60,59 +61,59 @@ int zero_comparation (float x)
 {
     float approximate_zero = 1e-5;
 
-    if (fabs(x) < approximate_zero)
+    if (fabs (x) < approximate_zero)
         return 0;
     else return 1;
 }
 
-void linear_solver (float b,float c,int *p_SolutionsAmount, SSolutions  *p_solution )
+void linear_solver (float b,float c, SSolutions  *p_solution)
 {
-    if ((zero_comparation(b) == 0) && (zero_comparation(c) == 0) )
-        *p_SolutionsAmount = InfiniteSolutions;
-    else if (zero_comparation(b) == 0)
-        *p_SolutionsAmount = NoSolutions;
-    else if (zero_comparation(c) == 0)
+    if ((zero_comparation (b) == 0) && (zero_comparation (c) == 0) )
+        (*p_solution).n = InfiniteSolutions;
+    else if (zero_comparation (b) == 0)
+        (*p_solution).n = NoSolutions;
+    else if (zero_comparation (c) == 0)
     {
-        *p_SolutionsAmount = OneSolution;
+        (*p_solution).n = OneSolution;
         (*p_solution).x1 = 0;
     }
     else
     {
-        *p_SolutionsAmount = OneSolution;
+        (*p_solution).n = OneSolution;
         (*p_solution).x1 = -c/b;
     }
 }
 
-void square_solver (float a,float b,float c, int *p_SolutionsAmount,SSolutions *p_solution)
+void square_solver (float a,float b,float c, SSolutions *p_solution)
 {
     float discriminant = b*b - 4*a*c;
     float doubled_a = 2*a;
-    float sqr_discr = sqrt(discriminant);
+    float sqr_discr = sqrt (discriminant);
 
     if (discriminant < 0)
-        *p_SolutionsAmount = NoSolutions;
-    else if (zero_comparation(discriminant) == 0)
+        (*p_solution).n = NoSolutions;
+    else if (zero_comparation (discriminant) == 0)
     {
-        *p_SolutionsAmount = OneSolution;
+        (*p_solution).n = OneSolution;
         (*p_solution).x1 = -b/doubled_a;
     }
     else
     {
-        *p_SolutionsAmount = TwoSolutions;
+        (*p_solution).n = TwoSolutions;
         (*p_solution).x1 = (-b - sqr_discr)/doubled_a ;
         (*p_solution).x2 = (-b + sqr_discr)/doubled_a;
     }
 }
 
-void solutions_output (int SolutionsAmount, SSolutions solution)
+void solutions_output (SSolutions solution)
 {
-    switch(SolutionsAmount)
+    switch (solution.n)
     {
     case OneSolution :
         printf ("There is one solution: %f", solution.x1);
         break;
     case TwoSolutions :
-        printf ("There are two solutios: %f and %f", solution.x1, solution.x2);
+        printf ("There are two solutions: %f and %f", solution.x1, solution.x2);
         break;
     case NoSolutions :
         printf ("There are no solutions");
