63be47ebf08822eedd198e138fdae6dcf050cc5a
Mikhail038 <111287669+Mikhail038@users.noreply.github.com>
2022-08-16 16:51:50 +0300

    Add files via upload

---
+++ Main_EquationSolver.cpp
@@ -0,0 +1,39 @@
+//Main File
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#include "Core_EquationSolver.h"
+#include "Core_EquationSolver.c"
+
+
+
+int main (void)
+{
+    Solutions solution;
+    solution.x1 = 0; solution.x2 = 0; solution.num = NoSolutions;
+    float a = 0, b = 0, c = 0;
+
+    coefficients_input (&a, &b, &c);
+
+    if (is_zero(a))
+        linear_solver (b, c, &solution);
+    else
+        square_solver (a, b, c, &solution);
+
+    solutions_output (&solution);
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
