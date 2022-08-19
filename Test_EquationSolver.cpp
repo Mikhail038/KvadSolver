d74ec1d7cdce2582d8548b91e35a858379e6140f
Mikhail038 <111287669+Mikhail038@users.noreply.github.com>
2022-08-19 19:42:09 +0300

    Проведена рестурктуризация тестов, добавлены ассерты

--- Test_EquationSolver.cpp
+++ Test_EquationSolver.cpp
@@ -1,73 +1,61 @@
 //Test File
 
 
-#include "Core_EquationSolver.h"
-#include "Core_EquationSolver.cpp"
-
 #ifndef TEST_EQUATION_SOLVER_CPP
 #define TEST_EQUATION_SOLVER_CPP
+
+#include "Core_EquationSolver.h"
+#include "Core_EquationSolver.cpp"
 
 
 void unittest()
 {
-    FILE *OutputFile = NULL, *InputFile = NULL;
+    FILE *OutputFile = NULL,
+    *InputFile = NULL;
 
-    enum TestCases StratCheckRes = NoFile;
-
-    TestAnswers answers = {}; TestAnswers *p_answers = &answers;
+    TestAnswers answers = {};
+    TestAnswers *p_answers = &answers;
 
     InputFile = fopen ("COEFS.txt", "r");
+    assert (InputFile != NULL);
 
     int TestsNumber = -1;
     fscanf (InputFile, "%d", &TestsNumber);
+    assert (TestsNumber >= 0);
 
     char junk1[100] = {};
     fgets (junk1, 100, InputFile);
 
-    if (TestsNumber == -1)
-    {
-        StratCheckRes = NoFile;
+    OutputFile = fopen ("RESULTS.txt", "w");
+    assert (OutputFile != NULL);
 
-        exam (InputFile, OutputFile, StratCheckRes, p_answers);
-    }
-    else if (TestsNumber == 0)
-    {
-        StratCheckRes = NoCoefs;
+    EquationTests equtest = {};
+    EquationTests *p_equtest = &equtest;
 
-        exam (InputFile, OutputFile, StratCheckRes, p_answers);
-    }
-    else
+    for (int testnumber = 0; testnumber < TestsNumber; testnumber++)
+    {
+        if (input (InputFile, p_equtest))
         {
-            StratCheckRes = Normal;
-
-            OutputFile = fopen ("RESULTS.txt", "w");
-
-            EquationTests equtest = {}; EquationTests *p_equtest = &equtest;
-
-            for (int testnumber=0; testnumber<TestsNumber; testnumber++)
-            {
-                if (input (InputFile, p_equtest) == INPUT_RETURN)
-                {
-                    choose_master (OutputFile, p_equtest, testnumber, p_answers);
-                }
-                else
-                {
-                    fprintf (OutputFile, "Test ¹%d  WRONG DATA!!!\n", testnumber + 1);
-
-                    char junk2[100] = {};
-                    fgets (junk2, 100, InputFile);
-
-                    p_answers->all++;
-                }
-            }
-
-            exam (InputFile, OutputFile, StratCheckRes, p_answers);
+            p_answers->ok += test_master (OutputFile, p_equtest, testnumber);
         }
+        else
+        {
+            fprintf (OutputFile, "Test #%d  WRONG DATA!!!\n", testnumber + 1);
+
+            char junk2[100] = {};
+            fgets (junk2, 100, InputFile);
+        }
+        p_answers->all++;
+    }
+    fclose (InputFile);
+    fclose (OutputFile);
+
+    printf ("TESTS RESULT: %d OF %d RIGHT ANSWERS", p_answers->ok, p_answers->all);
 }
 
 bool check_answers (FILE *OutputFile, Solutions *p_mastersol, EquationTests *p_equtest,int testnumber)
 {
-    fprintf (OutputFile, "Test ¹%d  RIGHT ANSWERS: %d %f %f PROGRAMM ANSWERS: %d %f %f ", testnumber + 1,
+    fprintf (OutputFile, "Test #%d  RIGHT ANSWERS: %d %f %f PROGRAMM ANSWERS: %d %f %f ", testnumber + 1,
     p_mastersol->num, p_mastersol->x1, p_mastersol->x2, p_equtest->tnum, p_equtest->tx1, p_equtest->tx2);
 
     if ( ((p_mastersol->x1 == p_equtest->tx1) && (p_mastersol->x2 == p_equtest->tx2) && (p_mastersol->num == p_equtest->tnum)) ||
@@ -83,74 +71,34 @@ bool check_answers (FILE *OutputFile, Solutions *p_mastersol, EquationTests *p_e
     }
 }
 
-int input (FILE *InputFile, EquationTests *p_equtest)
+bool input (FILE *InputFile, EquationTests *p_equtest)
 {
-    int marker = fscanf (InputFile, "%f %f %f %d %f %f", &(p_equtest->ta), &(p_equtest->tb),
-    &(p_equtest->tc), &(p_equtest->tnum), &(p_equtest->tx1), &(p_equtest->tx2));
-
-    if (marker == INPUT_RETURN)
+    if (fscanf (InputFile, "%f %f %f %d %f %f", &(p_equtest->ta), &(p_equtest->tb),
+    &(p_equtest->tc), &(p_equtest->tnum), &(p_equtest->tx1), &(p_equtest->tx2)) == 6)
     {
         char junk3[100] = {};
         fgets (junk3, 100, InputFile);
+        return true;
     }
-
-    return marker;
+    return false;
 }
 
-void choose_master (FILE *OutputFile, EquationTests *p_equtest,int testnumber, TestAnswers *p_answers)
+int test_master (FILE *OutputFile, EquationTests *p_equtest, int testnumber)
 {
+    Solutions mastersol = {};
+    Solutions *p_mastersol = &mastersol;
+
     if (is_zero(p_equtest->ta))
-    {
-        p_answers->ok += linear_test_master (OutputFile, p_equtest, testnumber);
-        p_answers->all++;
-    }
-    else
-    {
-        p_answers->ok += square_test_master (OutputFile, p_equtest, testnumber);
-        p_answers->all++;
-    }
-}
-
-int linear_test_master (FILE *OutputFile, EquationTests *p_equtest, int testnumber)
-{
-    Solutions mastersol = {}; Solutions *p_mastersol = &mastersol;
-
-    linear_solver (p_equtest->tb, p_equtest->tc, p_mastersol);
+        {
+            linear_solver (p_equtest->tb, p_equtest->tc, p_mastersol);
+        }
+        else
+            square_solver (p_equtest->ta, p_equtest->tb, p_equtest->tc, p_mastersol);
 
     if (check_answers (OutputFile, p_mastersol, p_equtest, testnumber))
         return 1;
     else
         return 0;
-}
-
-int square_test_master (FILE *OutputFile, EquationTests *p_equtest, int testnumber)
-{
-    Solutions mastersol = {}; Solutions *p_mastersol = &mastersol;
-
-    square_solver (p_equtest->ta, p_equtest->tb, p_equtest->tc, p_mastersol);
-    if (check_answers (OutputFile, p_mastersol, p_equtest, testnumber))
-        return 1;
-    else
-        return 0;
-}
-
-void exam (FILE *InputFile, FILE *OutputFile, int StartCheckRes, TestAnswers *p_answers)
-{
-    fclose (InputFile);
-    fclose (OutputFile);
-    switch (StartCheckRes)
-    {
-    case NoFile:
-        printf ("TEST FAILED: FILE NOT FOUND OR CORRUPTED  <COEFS.txt> ");
-        break;
-    case NoCoefs:
-        printf ("TEST FAILED: NO COEFFICIENTS FOUND IN <COEFS.txt> ");
-        break;
-    case Normal:
-        printf ("TESTS RESULT: %d OF %d RIGHT ANSWERS", p_answers->ok, p_answers->all);
-        break;
-    }
 }
 
 #endif // TEST_EQUATION_SOLVER_CPP
-
